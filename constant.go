7b72d23aa85b1717528ebe8753160b7ac3201f4a
azizhadiid <azizalhadiid88@gmail.com>
2025-09-01 20:49:49 +0700

    Learn Data Type and Variable

---
+++ constant.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	const (
+		firstName string = "Aziz"
+		lastName         = "Alhadiid"
+	)
+
+	fmt.Println(firstName)
+	fmt.Println(lastName)
+}
---
+++ variable.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string
+
+	name = "Aziz Alhadiid"
+	fmt.Println(name)
+}
